a75605bcba8618e47c64e139d36ef1f7bdd6ec35
Anoop Pemmaraju <anuppemmaraju@outlook.com>
2026-08-23 21:53:36 +0530

    Creating and Reading Signals

--- apm-begin/src/app/products/product-selection/product-selection.ts
+++ apm-begin/src/app/products/product-selection/product-selection.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductData } from '../product-data';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-selection',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './product-selection.html',
-  styleUrl: './product-selection.css'
+  styleUrl: './product-selection.css',
 })
 export class ProductSelection {
   pageTitle = 'Product Selection';
+  quantity = signal(1);
+  selectedProduct = signal<Product | undefined>(undefined);
+  products = signal(ProductData.products);
 
+  onIncrease() {
+    // this.quantity.set() //sets specific value
+    this.quantity.update((q) => q + 1); //sets dynamic values
+  }
+
+  onDecrease() {
+    this.quantity.update((q) => (q <= 0 ? 0 : q - 1));
+  }
+
+  qtyEffect = effect(() => console.log('quantity', this.quantity()));
 }
